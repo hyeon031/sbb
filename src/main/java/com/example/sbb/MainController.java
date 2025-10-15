c16cac343ba5cfdd713b72a5d8c72cc90442f353
hyeon <ejrgisv12@gmail.com>
2025-10-16 00:35:15 +0900

    질문 엔티티 만들기

---
+++ src/main/java/com/example/sbb/MainController.java
@@ -0,0 +1,14 @@
+package com.example.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class MainController {
+    @GetMapping("/sbb")
+    public String sbb(){
+        return "sbb";
+    }
+}
