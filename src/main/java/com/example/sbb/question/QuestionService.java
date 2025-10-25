f2b1e21a2e39d9a5e1dffb46e75b578a0ba4480e
hyeon <ejrgisv12@gmail.com>
2025-10-25 19:55:59 +0900

    답변 컨트롤러 만들기

--- src/main/java/com/example/sbb/question/QuestionService.java
+++ src/main/java/com/example/sbb/question/QuestionService.java
@@ -1,11 +1,11 @@
 package com.example.sbb.question;
 
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
 import lombok.RequiredArgsConstructor;
 
+import java.util.Optional;
+import com.example.sbb.DataNotFoundException;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +15,14 @@ public class QuestionService {
 
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        } else{
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
