8b8258da084dd618fc9c6681679c59ac85e3f76b
hyeon <ejrgisv12@gmail.com>
2025-10-25 20:02:41 +0900

    답변 서비스 만들기

--- src/main/java/com/example/sbb/answer/AnswerController.java
+++ src/main/java/com/example/sbb/answer/AnswerController.java
@@ -18,11 +18,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AnswerController {
 
     private final QuestionService questionService;
+    private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value = "content") String content){
         Question question = this.questionService.getQuestion(id);
-        // TODO: 답변을 저장한다.
+        this.answerService.create(question, content); // TODO: 답변을 저장한다.
         return String.format("redirect:/question/detail/%s", id);
     }
 }
