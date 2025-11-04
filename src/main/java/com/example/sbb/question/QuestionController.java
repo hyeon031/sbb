c4c2f63a6b2e457e25b6b8fec1feed27a322e6ff
hyeon <ejrgisv12@gmail.com>
2025-11-04 18:42:37 +0900

    페이징 기능 추가하기

--- src/main/java/com/example/sbb/question/QuestionController.java
+++ src/main/java/com/example/sbb/question/QuestionController.java
@@ -12,7 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import lombok.RequiredArgsConstructor;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.Page;
 
 
 @RequestMapping("/question")
@@ -23,9 +24,10 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/list")
-    public String list(Model model){
-        List<Question> questionList = this.questionService.getList();
-        model.addAttribute("questionList", questionList);
+    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page){
+        Page<Question> paging = this.questionService.getList(page);
+//        List<Question> questionList = this.questionService.getList();
+        model.addAttribute("paging", paging);
         return "question_list";
     }
 
--- src/main/java/com/example/sbb/question/QuestionService.java
+++ src/main/java/com/example/sbb/question/QuestionService.java
@@ -1,10 +1,18 @@
 package com.example.sbb.question;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.data.domain.Sort;
+
 import java.util.List;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import java.time.LocalDateTime;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 import com.example.sbb.DataNotFoundException;
@@ -34,5 +42,12 @@ public class QuestionService {
         q.setContent(content);
         q.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q);
+    }
+
+    public Page<Question> getList(int page){
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page,10,Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
 }
