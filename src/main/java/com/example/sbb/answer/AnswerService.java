97afa3597dcaaa5244729e5c58f964e9515e321c
hyeon <ejrgisv12@gmail.com>
2025-11-22 20:35:19 +0900

    수정, 삭제 기능 추가

--- src/main/java/com/example/sbb/answer/AnswerService.java
+++ src/main/java/com/example/sbb/answer/AnswerService.java
@@ -1,5 +1,7 @@
 package com.example.sbb.answer;
 
+import com.example.sbb.DataNotFoundException;
+import java.util.Optional;
 import com.example.sbb.user.SiteUser;
 import com.example.sbb.question.Question;
 import lombok.RequiredArgsConstructor;
@@ -21,5 +23,24 @@ public class AnswerService {
         answer.setAuthor(author);
         this.answerRepository.save(answer);
         return answer;
+    }
+
+    public Answer getAnswer(Integer id){
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if(answer.isPresent()){
+            return answer.get();
+        } else{
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content){
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer){
+        this.answerRepository.delete(answer);
     }
 }
