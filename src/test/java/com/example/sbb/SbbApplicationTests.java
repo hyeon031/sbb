6ed8cb2d6ead9d22e0debc4d5ed012973ed85c7b
ejrgi <ejrgisv12@gmail.com>
2025-11-26 20:00:35 +0900

    대댓글 기능 추가0

--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -30,7 +30,7 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa(){
-		for(int i=0; i<=300; i++){
+		for(int i=0; i<=100; i++){
 			String subject = String.format("테스트 데이터입니다 : [%03d]", i);
 			String content = "내용무";
 			this.questionService.create(subject, content, null);
